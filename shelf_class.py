6072a90028db765adb33088e74d3e0889a10ccd6
exo-cortex <lukipuki@physik.tu-berlin.de>
2021-07-08 16:10:43 +0200

    Moving everything into a dedicated python class.

---
+++ shelf_class.py
@@ -0,0 +1,45 @@
+
+class Shelf:
+	# constants
+	THICKNESS
+	WIDTH
+	HEIGHT
+	DEPTH
+	EXTRA_DEPTH
+
+	levels = 0 # number of vertical compartments
+	level_heights = [] # heights of each vertical compartment
+	level_separators = [] # absolute positions
+	compartment_widths = [] # for each level a list with widths
+	compartment_separators = [] # list of absolute positions 
+
+	# for building
+	horizontal_boards = []
+	vertical_boards = []
+	extension_boards []
+
+	# for plotting / visualizing
+	horizaontal_board_coordinates = []
+	vertical_board_coordinates = []
+
+
+	def __init__(self, thickness=thickness):
+
+		horizontal_board_positions = [0]
+
+	def add_level(_height):
+		# check if heights + THICKNESS-es are lower than HEIGHT
+		level_heights += [_height]
+
+	def add_levels(_heightsList):
+		level_heights += [_heightsList]
+
+	def add_compartment(_at_level, _width):
+
+	def add_compartment_list(_at_level, _widthsList):
+
+	def printWeight():
+
+	def shuffleLevels():
+	
+	def shuffleCompartments(_at_Level): # if nothing given shuffle at all levels
